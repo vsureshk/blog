ea54c2de90a535724032c342e6728682463f3083
gtpl <gtpl@gtpl-MS-7142.(none)>
2012-08-20 12:26:56 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'de22b2865a281a71d97898b06a52f136ec1dc60f8908a5a19b61689e727d18f6aa25e7e32d786d86daa405c6d05311dd773053cc91ec58a7a5570fa53ea83791'
